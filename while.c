68aea21464384de49f87255b47bf96f8d649a855
Nihal-Prakash <nihalprakash314@gmail.com>
2024-05-21 00:55:11 +0530

    created and update while.c

---
+++ while.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int i1 = 3;
+    while (i1 > 0) {
+        printf("hi\n");
+        i1--;
+    }
+
+    printf("\n");
+
+    int i2 = 0;
+    while (i2 < 3) {
+        printf("hello\n");
+        i2++;
+    }
+
+}
