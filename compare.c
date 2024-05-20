5e2fb38910e7266d7f6fc450ce4d0f2537ca6aee
Nihal-Prakash <nihalprakash314@gmail.com>
2024-05-21 00:47:59 +0530

    Updated comapre.c

--- compare.c
+++ compare.c
@@ -3,8 +3,18 @@
 int main(void) {
     int x = 5;
     int y = 7;
+
     if (x < y){
-        printf("x is less than y");
+
+        printf("x is less than y\n");
+
+    } else if (x == y)
+    {
+        printf("x is equal to y\n");
+
+    } else {
+        
+        printf("x is more than y\n");
     }
 
 }
