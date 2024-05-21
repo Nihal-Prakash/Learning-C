59e610df9bc0b9ca9a632d61b1f66638c05d96e5
Nihal-Prakash <nihalprakash314@gmail.com>
2024-05-21 13:47:35 +0530

    Update Blocks.c, minor commit

--- blocks.c
+++ blocks.c
@@ -3,7 +3,8 @@
 int main (void) {
     const int n = 5;
     for (int i = 0; i < n; i++){
-            for ( int j = 0; j < n+2; j++) //added 2 so that it kinda looks more like a sqaure
+            //added 2 so that it kinda looks more like a sqaure
+            for ( int j = 0; j < n+2; j++) 
             {
                 printf("#");
             };
